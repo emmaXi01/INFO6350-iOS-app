f1a27800f4da66c2da83c906068b2791c27b15f9
EmmaXi <xxmhzau@gmail.com>
2021-01-26 11:51:14 -0800

    Initial Commit

---
+++ INFO6350App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INFO6350App
+//
+//  Created by 席晓敏 on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
